51c4cac53a5efc546c0e89a8340e592606f172f1
John Zeng <johnzeng878@gmail.com>
2024-07-15 16:41:17 -0700

    add bindings

--- src/sim.py
+++ src/sim.py
@@ -1,21 +1,45 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
-
+from textual.containers import ScrollableContainer
+from textual.widgets import Header, Footer, Button, Static
+from time import sleep
 
 class RugbySimulator(App):
     """A Textual app to manage stopwatches."""
 
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"), # inherited from App
+        ("q", "quit", "Quit the app"), # inherited from App
+        ("r", "run_sim", "Run the simulation"),
+        (")","increase_speed","Increase the speed of simulation"),
+        ("(","decrease_speed","Decrease the speed of simulation"),
+        ("shift+space","pause","Pause the simulation"),
+        ("shift+enter","resume","Resume the simulation"),
+        ("s", "stop", "Stop the simulation"),
+        ]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
 
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.dark = not self.dark
+    def action_run_sim(self):
+        """Run the simulation."""
+        
+    def action_increase_speed(self):
+        """Increase the speed of the simulation."""
+    
+    def action_decrease_speed(self):
+        """Decrease the speed of the simulation."""
+    
+    def action_pause(self):
+        """Pause the simulation."""
 
+    def action_resume(self):
+        """Resume the simulation."""
+
+    def action_stop(self):
+        """Stop the simulation."""
+        
 
 if __name__ == "__main__":
     app = RugbySimulator()
