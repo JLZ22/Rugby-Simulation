181858ef9bcdd91ba52859015cd22e1fc15b3156
John Zeng <johnzeng878@gmail.com>
2024-07-15 15:46:41 -0700

    create basic textual app

---
+++ src/sim.py
@@ -0,0 +1,22 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+
+class RugbySimulator(App):
+    """A Textual app to manage stopwatches."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+
+if __name__ == "__main__":
+    app = RugbySimulator()
+    app.run()
