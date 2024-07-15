785415b1cb184ace310a5a693ad65c0b52c22ee1
John Zeng <johnzeng878@gmail.com>
2024-07-15 10:57:28 -0700

    update run to improve convenience of args

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import Drill
-
-def main():
-    drill = Drill.Drill(2,5)
-    drill.runDrill(20)
-    drill.printOsicllations()
-
-if __name__ == '__main__':
-    main()
--- src/run_drill.py
+++ src/run_drill.py
@@ -3,9 +3,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Run a drill.')
-    parser.add_argument('--num-lines', type=int, help='Number of lines', default=3)
-    parser.add_argument('--num-players', type=int, help='Number of players', default=9)
-    parser.add_argument('--num-passes', type=int, help='Number of passes', default=20)
+    parser.add_argument("Lines", type=int, help='Number of lines', default=3)
+    parser.add_argument("Players", type=int, help='Number of players', default=9)
+    parser.add_argument("-p", type=int, help='Number of passes', default=40)
     args = parser.parse_args()
 
     drill = Drill(args.num_lines, args.num_players)
